5cfcee7f37bf68ed0d333f6acf338d13f408a732
Aurelien Jarno <aurelien@aurel32.net>
2019-06-22 17:25:44 +0200

    sync.sh: use the same rsync options for the mirrors and for the buildds
    Some options are a bit overkill for the buildds, but the maintenance is
    easier that way.

--- bin/sync.sh
+++ bin/sync.sh
@@ -18,10 +18,7 @@ TARGET="$TARGET_BASE/$1"
 PASSWORD_BASE=/srv/wanna-build/etc
 PASSWORD_FILE="$PASSWORD_BASE/$1.rsync-password"
 
-RSYNC_OPTIONS="--delete --delete-excluded -av"
-
-MIRROR_EXCLUDES="--exclude=**/*.changes --exclude=**/installer-* --exclude=**/Packages.diff --exclude=**/Sources.diff --exclude=ChangeLog --exclude=**/Contents-* --exclude=**/Translation-* --exclude=**/*.bz2 --exclude=Packages --exclude=Sources --exclude=**/*.new" # the latter two because we only accept .gz or .xz files
-MIRROR_OPTIONS="$MIRROR_EXCLUDES $RSYNC_OPTIONS"
+RSYNC_OPTIONS="--delete --delete-excluded -av --exclude=**/*.changes --exclude=**/installer-* --exclude=**/Packages.diff --exclude=**/Sources.diff --exclude=ChangeLog --exclude=**/Contents-* --exclude=**/Translation-* --exclude=**/*.bz2 --exclude=Packages --exclude=Sources --exclude=**/*.new" # the latter two because we only accept .gz or .xz files
 
 export RSYNC_SSL_METHOD=socat
 export RSYNC_SSL_CAPATH=/etc/ssl/ca-debian
@@ -52,25 +49,22 @@ fi
 case $1 in
 debian)
 	USER=buildd.debian.org
-	BUILDD_QUEUE_OPTIONS="--exclude=*.bz2 $RSYNC_OPTIONS"
-	$RSYNC --password-file "$PASSWORD_FILE" $MIRROR_OPTIONS $USER@ftp-master.debian.org::debian/dists/ "$TARGET/archive"
-	$RSYNC --password-file "$PASSWORD_BASE/$1-buildd.rsync-password" $BUILDD_QUEUE_OPTIONS $USER@ftp-master.debian.org::debian-buildd-dists/ "$TARGET/debian-buildd-dists/"
+	$RSYNC --password-file "$PASSWORD_FILE" $RSYNC_OPTIONS $USER@ftp-master.debian.org::debian/dists/ "$TARGET/archive"
+	$RSYNC --password-file "$PASSWORD_BASE/$1-buildd.rsync-password" $RSYNC_OPTIONS $USER@ftp-master.debian.org::debian-buildd-dists/ "$TARGET/debian-buildd-dists/"
 	# Also sync the Maintainers and Uploaders files for consumption through the web interface.
-	$RSYNC --password-file "$PASSWORD_FILE" $MIRROR_OPTIONS $USER@ftp-master.debian.org::debian/indices/Maintainers /srv/buildd.debian.org/etc/Maintainers
-	$RSYNC --password-file "$PASSWORD_FILE" $MIRROR_OPTIONS $USER@ftp-master.debian.org::debian/indices/Uploaders /srv/buildd.debian.org/etc/Uploaders
+	$RSYNC --password-file "$PASSWORD_FILE" $RSYNC_OPTIONS $USER@ftp-master.debian.org::debian/indices/Maintainers /srv/buildd.debian.org/etc/Maintainers
+	$RSYNC --password-file "$PASSWORD_FILE" $RSYNC_OPTIONS $USER@ftp-master.debian.org::debian/indices/Uploaders /srv/buildd.debian.org/etc/Uploaders
 	;;
 debian-ports)
 	USER=buildd.debian.org
-	BUILDD_QUEUE_OPTIONS="--exclude=*.bz2 $RSYNC_OPTIONS"
-	$RSYNC --password-file "$PASSWORD_FILE" $MIRROR_OPTIONS $USER@ports-master.debian.org::debian-ports/dists/ "$TARGET/archive"
-	$RSYNC --password-file "$PASSWORD_BASE/$1-buildd.rsync-password" $BUILDD_QUEUE_OPTIONS $USER@ports-master.debian.org::debian-ports-buildd-dists/ "$TARGET/debian-ports-buildd-dists/"
+	$RSYNC --password-file "$PASSWORD_FILE" $RSYNC_OPTIONS $USER@ports-master.debian.org::debian-ports/dists/ "$TARGET/archive"
+	$RSYNC --password-file "$PASSWORD_BASE/$1-buildd.rsync-password" $RSYNC_OPTIONS $USER@ports-master.debian.org::debian-ports-buildd-dists/ "$TARGET/debian-ports-buildd-dists/"
 	;;
 debian-security)
 	chmod 0700 "$TARGET"
 	USER=buildd.debian.org
-	BUILDD_QUEUE_OPTIONS="--exclude=*.bz2 $RSYNC_OPTIONS"
-	$RSYNC --password-file "$PASSWORD_BASE/$1.rsync-password" $MIRROR_OPTIONS $USER@security-master.debian.org::debian-security/dists/ "$TARGET/archive"
-	$RSYNC --password-file "$PASSWORD_BASE/$1-buildd.rsync-password" $BUILDD_QUEUE_OPTIONS $USER@security-master.debian.org::debian-security-buildd-dists/ "$TARGET/debian-security-buildd-dists/"
+	$RSYNC --password-file "$PASSWORD_BASE/$1.rsync-password" $RSYNC_OPTIONS $USER@security-master.debian.org::debian-security/dists/ "$TARGET/archive"
+	$RSYNC --password-file "$PASSWORD_BASE/$1-buildd.rsync-password" $RSYNC_OPTIONS $USER@security-master.debian.org::debian-security-buildd-dists/ "$TARGET/debian-security-buildd-dists/"
 	;;
 *)
 	echo "Sync target $1 not supported, aborting."
